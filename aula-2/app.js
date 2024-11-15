4c3703264842ae9ac1299ffb7517c20844295841
Gabriel <gapereiradesigner@gmail.com>
2024-11-14 23:31:02 -0300

    exercícios da aula 2

--- aula-2/app.js
+++ aula-2/app.js
@@ -1,9 +1,47 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
-    console.log(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você Errrrrouu!!!')
-}
+// if (chute == numeroSecreto) {
+//     console.log(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+// } else {
+//     alert('Você Errrrrouu!!!')
+// }
+
+// aula 1
+// let respostaUser = prompt('Que dia é hoje?')
+
+// let diaSemana = respostaUser;
+
+// if(respostaUser === 'Sábado' || respostaUser === 'Domingo') {
+//     alert(`Bom final de semana`)
+// } else {
+//     alert('Boa semana!!')
+// }
+
+// aula 2
+// let numeroUser = parseInt(prompt('Diga teu número'));
+
+// if(numeroUser >= 0) {
+//     alert('positivo capitão')
+// } else {
+//     alert('negas')
+// }
+
+// aula 3
+// let pontosJogo = parseInt(prompt('Insira seus pontos'))
+
+// if(pontosJogo >= 100) {
+//     alert('parabains sindy parabains')
+// } else {
+//     alert('vc eh horroroso pqpkkkkkkkk')
+// }
+
+// aula 4
+// let saldoConta = 'R$100,00'
+// alert(`Pobre imundo vc tem apenas ${saldoConta}`);
+
+// aula 5
+// let nomeUser = prompt('Digite seu nome');
+
+// alert('Boas-vindas ' + nomeUser);
