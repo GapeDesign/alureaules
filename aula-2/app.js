aeb974bbb8e926af947058c3b48cedc4dba5b05c
Gabriel <gapereiradesigner@gmail.com>
2024-11-14 22:38:23 -0300

    ajustes da aula 2

--- aula-2/app.js
+++ aula-2/app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    console.log(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você Errrrrouu!!!')
 }
