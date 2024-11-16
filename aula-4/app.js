9934b0f827d353851b126491c06cbccefa6ddc62
Gabriel <gapereiradesigner@gmail.com>
2024-11-16 12:39:30 -0300

    exercicios mais dificeis que o mundo da programação pós-ditadura já viu. Infelizmente.

--- aula-4/app.js
+++ aula-4/app.js
@@ -1,28 +1,94 @@
-alert('Boas vindas ao jogo do número secreto');
+// alert('Boas vindas ao jogo do número secreto');
 
-const numeroMaximo = 10;
-const numeroAleatorio = Math.random();
-const entreZeroEnumeroMaximo = numeroAleatorio * numeroMaximo;
+// const numeroMaximo = 10;
+// const numeroAleatorio = Math.random();
+// const entreZeroEnumeroMaximo = numeroAleatorio * numeroMaximo;
 
 
-let numeroSecreto = parseInt(entreZeroEnumeroMaximo + 1); 
-console.log(numeroSecreto);
-let chute;
-let numeroDeTentativas = 1;
+// let numeroSecreto = parseInt(entreZeroEnumeroMaximo + 1); 
+// console.log(numeroSecreto);
+// let chute;
+// let numeroDeTentativas = 1;
 
-// enquanto o chute não for igual ao n.s.
-while(chute != numeroSecreto) {
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
-    if(chute === numeroSecreto) {
-    let palavraTentativa = numeroDeTentativas > 1 ? "tentativas" : "tentativa"; 
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${numeroDeTentativas} ${palavraTentativa}`);
-    } else {
-        if(chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
-        } else {
-            alert(`O número secreto é maior que ${chute}`)
-        }
-    }
-    numeroDeTentativas++;
-}
+// // enquanto o chute não for igual ao n.s.
+// while(chute != numeroSecreto) {
+//     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+//     // se chute for igual ao número secreto
+//     if(chute === numeroSecreto) {
+//     let palavraTentativa = numeroDeTentativas > 1 ? "tentativas" : "tentativa"; 
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${numeroDeTentativas} ${palavraTentativa}`);
+//     } else {
+//         if(chute > numeroSecreto) {
+//             alert(`O número secreto é menor que ${chute}`)
+//         } else {
+//             alert(`O número secreto é maior que ${chute}`)
+//         }
+//     }
+//     numeroDeTentativas++;
+// }
+
+// exercicio 1
+// const mensagem = "Boas vindas";
+// console.log(mensagem);
+
+// exercicio 2
+// let nome = "Gabriel";
+// console.log(`Olá, ${nome}`);
+
+// exercício 3
+// let nome = "Gabriel";
+// console.log(`Olá, ${nome}`);
+
+// exercicio 4
+// let langPreferida = prompt("Qual a sua lang preferida???w")
+// console.log(langPreferida);
+
+// exercicio 5
+// let valor1 = 6;
+// let valor2 = 9;
+// let resultado = valor1 + valor2;
+// console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+// exercicio 6
+// let valor1 = 6;
+// let valor2 = 9;
+// let resultado = valor1 - valor2;
+// console.log(`A subtração de ${valor1} e ${valor2} é igual a ${resultado}.`);
+
+// exercicio 7
+// let idade = prompt("Digite sua idade:");
+// if(idade >= 18) {
+//     console.log("Você já pode ser preso!!");
+// } else {
+//     console.log("Menor de idade.");
+// }
+
+// exercicio 8
+// let numero = parseInt(prompt("Digite um número:"));
+// if(numero > 0) console.log("Número positivo");
+// else if(numero < 0) console.log("Número negativo");
+// else console.log("É zero");
+
+// exercicio 9
+// let contador = 1;
+// while(contador <= 10) {
+//     console.log(contador);
+//     contador++
+// }
+
+// exercicio 10
+// let nota = parseInt(prompt("Digite a sua nota:"));
+// const resultado = nota >= 7 ? "Você passou!" : "Você não passou..."
+// console.log(resultado);
+
+// exercicio 11
+// let numeroRandom = Math.random();
+// console.log(numeroRandom);
+
+// exercicio 12
+// let numeroRandom = parseInt(Math.random() * 10 + 1);
+// console.log(numeroRandom);
+
+// exercicio 13
+// let numeroRandom = parseInt(Math.random() * 1000 + 1);
+// console.log(numeroRandom);
